46239670b228d69129e660e9f09fb9d834f5ef0f
shadykh <eng.shady94@gmail.com>
2021-06-28 12:17:40 +0300

    Lab 31, done, v1.2, Fix DeployLink

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,16 +1,14 @@
 import React from "react";
-import { MDBContainer, MDBFooter } from "mdbreact";
-import 'mdb-react-ui-kit/dist/css/mdb.min.css'
 import './footer.scss';
 function Footer (){
     return (
-      <MDBFooter color="blue" className="font-small pt-4 mt-4">
+      <div color="blue" className="font-small pt-4 mt-4">
        <div className="footer-copyright text-center py-3">
-        <MDBContainer fluid>
+        <div fluid>
           &copy; {new Date().getFullYear()} Copyright: 401-JavaScript
-        </MDBContainer>
+        </div>
       </div>
-    </MDBFooter>
+    </div>
     );
   }
 
